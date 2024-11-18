4f97548f43928a580d99f808ad27482594de207f
Lucas da Rosa Nunes <llucas-nunes@hotmail.com>
2024-11-18 15:18:51 -0300

    initial commit

---
+++ src/Loja.java
@@ -0,0 +1,28 @@
+
+public class Loja {
+    /*atributos da loja */
+    String nome;
+    int quantidadeFuncionarios;
+    float salarioBaseFuncionario;
+
+    /*construtores da loja*/
+
+    //Construtor padrão -> Não inicia nenhum atributo
+    Loja() {
+
+    }
+
+    //Construtor com valores nome e qtd
+    Loja(String nome, int quantidadeFuncionarios) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = -1;
+    }
+
+    //Construtor completo
+    Loja(String nome, int quantidadeFuncionarios, float salarioBaseFuncionario) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Loja newSchool = new Loja();
+
+        newSchool.nome = "newSchool";
+        System.out.println(newSchool.nome);
+    }
+}
