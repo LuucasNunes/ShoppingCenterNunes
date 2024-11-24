d5b882bcdffcc78b754c40f8699badc49c81a38b
pc evil <llucas-nunes@hotmail.com>
2024-11-24 01:15:06 -0300

    Adicionando mais opções de criação da loja!

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
@@ -18,6 +19,8 @@ public class Principal {
             switch (lerInt) {
                 case 1:
                     int continuarCriando = ler.nextInt();
+                    ArrayList<Loja> lojas = new ArrayList<>();
+                    int lojaContador = 1;
                     do {
                         System.out.println("Você escolheu: Criar uma loja.\n");
                         System.out.println("Digite um nome para sua Loja: ");
@@ -33,13 +36,33 @@ public class Principal {
                         int mesCriaLoja = ler.nextInt();
                         System.out.println("\nDigite o ano da criação da loja: ");
                         int anoCriaLoja = ler.nextInt();
-                        System.out.println("\nVocê criou essa loja: \n" + "Nome da loja: " + nomeDaLoja
-                                + "\nQuantidade de funcionarios: " + qtdFunc
-                                + "\nSalário base dos funcionários: " + salaBaseFunc
-                                + "\nData da criação da loja: " + diaCriaLoja + "/" + mesCriaLoja + "/" + anoCriaLoja + "\n");
-                        System.out.println("Você deseja criar mais uma loja? (1)Sim (2)Não");
+                        System.out.println("\n!--------------------------!\n");
+                        System.out.println("\nDigite a rua da loja: ");
+                        String nomeDaRua = ler.nextLine();
+                        System.out.println("\nDigite o nome da cidade: ");
+                        String nomeDaCidade = ler.nextLine();
+                        System.out.println("\nDigite o país: ");
+                        String paisDaLoja = ler.nextLine();
+                        System.out.println("\nDigite o CEP da loja: ");
+                        String cepDaLoja = ler.nextLine();
+                        System.out.println("\nDigite o número da loja: ");
+                        String numDaLoja = ler.nextLine();
+                        System.out.println("\nDigite o complemento da loja: ");
+                        String compDaLoja = ler.nextLine();
+                        Data dataCriacao = new Data(diaCriaLoja, mesCriaLoja, anoCriaLoja);
+                        Endereco enderecoDaLoja = new Endereco(nomeDaRua, nomeDaCidade, paisDaLoja, cepDaLoja,  numDaLoja,compDaLoja);
+                        Loja loja = new Loja(nomeDaLoja, qtdFunc, salaBaseFunc, dataCriacao, enderecoDaLoja);
+                        lojas.add(loja);
+                        lojaContador++;
+                        System.out.println("Você criou essa loja: " + loja + "\n");
+                        System.out.println("\nVocê deseja criar mais uma loja? (1)Sim (2)Não");
                         continuarCriando = ler.nextInt();
                     } while (continuarCriando == 1);
+                    System.out.println("\nResumo de todas as lojas criadas:");
+                    for (int i = 0; i < lojas.size(); i++) {
+                        System.out.println("\nLoja " + (i + 1) + ":");
+                        System.out.println(lojas.get(i));  // Exibe cada loja criada
+                    }
                     break;
                 case 2:
                     System.out.println("Você escolheu: Criar um produto.");
